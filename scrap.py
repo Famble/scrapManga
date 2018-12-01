4e15391aa525a0d388eb3c37bc6c090ed066537a
Famble <Famble@users.noreply.github.com>
2018-12-01 18:22:17 +0100

    Working script, but needs some updates later

--- scrap.py
+++ scrap.py
@@ -1,3 +1,8 @@
+
+#https://chrisalbon.com/python/web_scraping/monitor_a_website/
+#https://www.adventuresintechland.com/detect-when-a-webpage-changes-with-python/
+#https://medium.freecodecamp.org/how-to-scrape-websites-with-python-and-beautifulsoup-5946935d93fe
+
 # Import requests (to download the page)
 import requests
 
@@ -7,20 +12,23 @@ from bs4 import BeautifulSoup
 # Import Time (to add a delay between the times the scape runs)
 import time
 
-# Import smtplib (to allow us to email)
+from pushbullet import Pushbullet
+pb = Pushbullet('API_key')
+
+
 
 # This is a pretty simple script. The script downloads the homepage of VentureBeat, and if it finds some text, emails me.
-# If it does not find some text, it waits 60 seconds and downloads the homepage again.
+
 
 # while this is true (it is true by default),
-chp_number = 27
+chp_number = "27"
 while True:
     # set the url as VentureBeat,
     url = "http://merakiscans.com/solo-leveling/" +chp_number +"/"
     print(url)
     # set the headers like we are a browser,
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    # download the homepage
+    # download the manga chapter page
     response = requests.get(url, headers=headers)
     print(response)
     # parse the downloaded homepage and grab all text, then,
@@ -28,29 +36,27 @@ while True:
 
     #name_box = soup.find('ul', attrs={'class': 'lst mng_chp'})
     #name = name_box.text.strip() # strip() is used to remove starting and trailing
-    #print name
-    #bol = False
 
+    checker = soup.find('div', attrs={'class': 'wpm_pag'}).find('b')
+    print(checker.text.strip())
+    
+    push = pb.push_link("New update for chapter: " + chp_number, url)
 
-
-    # if the number of times the word "Google" occurs on the page is less than 1,
-    if str(soup).find("Sorry, this chapter is not available yet!") == -1:
+# if the string is found
+    if checker.text.strip() == "Sorry, this chapter is not available yet!":
         print('No Update yet')
-        # wait 60 seconds,
-        time.sleep(60)
+        time.sleep(60*30) #check every 30min
         # continue with the script,
         continue
 
-    # but if the word "Google" occurs any other number of times,
+   
     else:
-        # create an email message with just a subject line,
-
-        print('I found something')
+        # send notification with pushbullet,
 
 
-        # send the email
-        # server.sendmail(fromaddr, toaddrs, msg)
-        # disconnect from the server
-        # server.quit()
+        push = pb.push_link("New update for chapter: " + chp_number, url)
+        print('I found an update')
+
+        time.sleep(60*60*18) #sleep for 18 hours
 
         break
